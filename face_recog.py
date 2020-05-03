0df32403308231181b592e8c47509e53d14b4fe0
prathameshap <prathameshpawar1301@gmail.com>
2020-05-02 18:31:00 -0700

    Scale factor 1.05 - Detects too many false positives Needs Work

--- face_recog.py
+++ face_recog.py
@@ -12,12 +12,12 @@ while(True):
     ret, frame = cap.read()
 
     frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
-   # cv2.imshow("Frame", frame)
+    #cv2.imshow("Frame", frame)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     face_cascade = cv2.CascadeClassifier(path)
 
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1, minNeighbors=5, minSize=(40,40))
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5, minSize=(40,40))
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0),2)
