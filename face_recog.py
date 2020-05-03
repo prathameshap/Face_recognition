7c821f4b103e4d41de9b2e5810ac8c20ebe8e68d
Prathamesh Pawar <47439425+prathameshap@users.noreply.github.com>
2020-05-02 18:19:17 -0700

    Add files via upload
    'Phase complete - Face detection using haarCascade'

---
+++ face_recog.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""we are acpturing live video from the camera"""
+
+import numpy as np
+import cv2
+
+cap  = cv2.VideoCapture(0)
+path = "haarcascade_frontalface_default.xml"
+
+while(True):
+
+    ret, frame = cap.read()
+
+    frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
+   # cv2.imshow("Frame", frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    face_cascade = cv2.CascadeClassifier(path)
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1, minNeighbors=5, minSize=(40,40))
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0),2)
+    cv2.imshow("Frame",frame)
+    ch = cv2.waitKey(1)
+    if ch & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
